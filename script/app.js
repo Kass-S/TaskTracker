1eaa691d3eb635164fe37a2ce18e0821dc1f39a4
Kass-S <kassiespacek101@gmail.com>
2025-02-14 11:35:53 -0800

    saved things populate on page

--- script/app.js
+++ script/app.js
@@ -11,16 +11,65 @@ let highPriorityBtn = document.getElementById('highPriorityBtn');
 
 let addTaskBtn = document.getElementById('addTaskBtn');
 
+let toDoStatus = document.getElementById('toDoStatus');
+
 let priorityStatus = 'Low'
 
+const GetTaskList = () => {
+    let taskList = getFromStorage();
+
+    taskList.map(task => {
+
+        let cardDiv = document.createElement('div');
+        cardDiv.className = "max-w-sm p-6 bg-amber-50 border border-gray-200 rounded-lg shadow-sm mt-5 m-5";
+
+        let cardH5 = document.createElement('h5');
+        cardH5.className = "mb-2 text-2xl font-bold tracking-tight";
+        cardH5.innerText = `${task.taskName}`;
+
+        let cardP = document.createElement('p');
+        cardP.className = "mb-3 font-normal text-gray-700";
+        cardP.innerText = `${task.taskDescription}`;
+
+        let cardPrirityP = document.createElement('p');
+        cardPrirityP.className = "mb-3 font-normal text-gray-700";
+        cardPrirityP.innerText = `Priority: ${task.priorityStatus}`;
+
+        let removeBtn = document.createElement('button');
+        removeBtn.className = 'bg-red-500 inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg  hover:bg-red-500 hover:cursor-pointer';
+        removeBtn.innerText = 'Delete';
+
+        let editBtn = document.createElement('button');
+        editBtn.className = "inline-flex items-center ml-4 px-5 py-2 text-sm font-medium text-center rounded-lg bg-green-500 hover:bg-green-600";
+        editBtn.innerText = "Edit";
+
+        removeBtn.addEventListener('click', () => {
+            removeFromStorage(task);
+            cardDiv.remove();
+        })
+
+        cardDiv.appendChild(cardH5);
+        cardDiv.appendChild(cardP);
+        cardDiv.appendChild(cardPrirityP);
+        cardDiv.appendChild(removeBtn);
+        cardDiv.appendChild(editBtn);
+
+        toDoStatus.appendChild(cardDiv);
+    })
+}
+GetTaskList();
+
 addTaskBtn.addEventListener('click', () => {
+    toDoStatus.innerHTML = '';
+
     let taskName = taskNameInput.value;
-    let taskDescriptionn = taskDescriptionInput.value;
+    let taskDescription = taskDescriptionInput.value;
     let dueDate = dueDateInput.value;
 
-    let wholeTask = {taskName, taskDescriptionn, dueDate, priorityStatus};
+    let wholeTask = {taskName, taskDescription, dueDate, priorityStatus};
     saveToStorage(wholeTask);
 
+    GetTaskList();
     taskNameInput.value = '';
     taskDescriptionInput.value = '';
     dueDateInput.value = '';
